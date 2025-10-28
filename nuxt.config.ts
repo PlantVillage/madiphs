a72df55087401d55349e291341e07a1f8c82b6e5
danathoodies <daniyal.gabitov@hoodies.team>
2025-10-28 21:45:20 +0400

    Add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -69,6 +69,17 @@ export default defineNuxtConfig({
     '@primevue/nuxt-module',
   ],
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-chrome-192x192.png' },
+      ],
+    },
+  },
   css: ['~/assets/css/main.css', 'primeicons/primeicons.css'],
   runtimeConfig: {
     public: {
